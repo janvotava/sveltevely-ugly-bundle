9d679d5653d4286e9471e60066e692378606ff5c
Jan Votava <votava@deployment.cz>
2023-05-15 13:27:45 +0700

    feat(TableBody): add class prop

--- src/lib/components/tables/TableBody.svelte
+++ src/lib/components/tables/TableBody.svelte
@@ -3,8 +3,11 @@
 	import type { ActionList } from 'svelte-useactions';
 
 	export let use: ActionList<HTMLDivElement> = [];
+
+	let clazz = '';
+	export { clazz as class };
 </script>
 
-<tbody class="divide-y divide-gray-200 dark:divide-gray-700" use:useActions={use}>
+<tbody class="divide-y divide-gray-200 dark:divide-gray-700 {clazz}" use:useActions={use}>
 	<slot />
 </tbody>
