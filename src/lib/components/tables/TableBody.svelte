237cc929c5fdf6688ab27d2cdf80a10701b0a91c
Jan Votava <votava@deployment.cz>
2023-05-15 13:26:30 +0700

    feat(TableBody): ability to pass actions to the tbody

--- src/lib/components/tables/TableBody.svelte
+++ src/lib/components/tables/TableBody.svelte
@@ -1,3 +1,10 @@
-<tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-  <slot />
+<script lang="ts">
+	import { useActions } from 'svelte-useactions';
+	import type { ActionList } from 'svelte-useactions';
+
+	export let use: ActionList<HTMLDivElement> = [];
+</script>
+
+<tbody class="divide-y divide-gray-200 dark:divide-gray-700" use:useActions={use}>
+	<slot />
 </tbody>
