8f79036a2be95e966db779a4de7389a35cdef017
Jan Votava <votava@deployment.cz>
2023-05-15 13:27:25 +0700

    feat(Table): add disabled prop

--- src/lib/components/tables/Table.svelte
+++ src/lib/components/tables/Table.svelte
@@ -2,10 +2,13 @@
 	let clazz = '';
 
 	export { clazz as class };
+	export let disabled = false;
 </script>
 
 <table
 	class="w-full bg-white rounded divide-gray-200 shadow-md table-fixed dark:divide-gray-700 dark:bg-slate-800 {clazz}"
+	class:opacity-50={disabled}
+	class:pointer-events-none={disabled}
 >
 	<slot />
 </table>
