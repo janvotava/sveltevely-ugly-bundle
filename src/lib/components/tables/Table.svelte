601e93728b3b829e572c59dd480d9a470de480c6
Jan Votava <votava@deployment.cz>
2023-05-15 11:19:25 +0700

    feat(Table): add class prop

--- src/lib/components/tables/Table.svelte
+++ src/lib/components/tables/Table.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	let clazz = '';
+
+	export { clazz as class };
+</script>
+
 <table
-  class="w-full bg-white rounded divide-gray-200 shadow-md table-fixed dark:divide-gray-700 dark:bg-slate-800"
+	class="w-full bg-white rounded divide-gray-200 shadow-md table-fixed dark:divide-gray-700 dark:bg-slate-800 {clazz}"
 >
-  <slot />
+	<slot />
 </table>
